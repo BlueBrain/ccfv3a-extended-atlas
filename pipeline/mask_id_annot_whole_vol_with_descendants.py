461b50c3e9fdd0ccd21e1bfec0915c601f4ddf9d
Sebastien-PILUSO <63786997+Sebastien-PILUSO@users.noreply.github.com>
2024-12-10 17:21:34 +0100

    Change to single

--- pipeline/mask_id_annot_whole_vol_with_descendants.py
+++ pipeline/mask_id_annot_whole_vol_with_descendants.py
@@ -11,7 +11,6 @@ import nrrd
 import sys
 import os
 
-
 # =============================================================================================
 
 
@@ -63,49 +62,31 @@ def write_img(img_path = "", out_array_img = []):
 
 
 
-def mask_id_annot(input_annot_file_path = "", input_annatomical_data_path = "", input_hier_id_folder = "", output_mask_folder = "", output_masked_annat_folder = ""):
+def mask_id_annot(input_annot_file_path = "", ex = "", output_file_path = ""):
     """
     Identifying the ID hierarchy 
     @method mask_id_annot
     @param {String} input_annot_file_path The path of the input anntation file
-    @param {String} input_annatomical_data_path The path of the anatomical file corresponding the the annotation file
-    @param {String} input_hier_id_folder The path of the input hierarchy id folder
-    @param {String} output_mask_folder The path of the output folder where to save the masks
-    @param {String} output_masked_annat_folder The path of the output folder where to save the anatomical volumes masked
-    @return {None} Writes the masked files with only the ids that have been selected
+    @param {String} input_hier_id_file_list The path of the input hierarchy id 
+    @param {String} output_file_path The path of the output mask file to be written
+    @return {None} Writes the masked file with only the ids that have been selected
     """
 
     # Reading the input file
     annot_vol = read_img(input_annot_file_path)
     annot_vol_dim = annot_vol.shape
-    annat_vol = read_img(input_annatomical_data_path)
-    id_list_full = [f for f in os.listdir(input_hier_id_folder)]
+    id_list = read_img(input_hier_id_file_list)
+    print("dtype:", annot_vol.dtype)
 
     # Setting all values to 0 except those in id_list that are set to 1
-    for id in id_list_full:
-        print("id:", id)
-        id_list = read_img(os.path.join(input_hier_id_folder, id))
-        for i in range(len(id_list)):
-            print("Processing id_nb " + str(i + 1) + "/" + str(len(id_list)) + "   |   ID " + str(id_list[i]))
+    output_vol = np.zeros(annot_vol_dim, dtype=annot_vol.dtype)
+    # for id in id_list:
+    #     coord = np.where(annot_vol == id)
+    #     output_vol[coord] = 1
+    output_vol[np.isin(annot_vol, id_list)] = 1
 
-            # Checking if aloredaay exists
-            output_masked_annat = os.path.join(output_masked_annat_folder, str(id_list[i]) + "_masked.nrrd")
-            if os.path.exists(output_masked_annat):
-                print("Skip: ID " + str(id_list[i]) + " already exists")
-                pass
-            else:
-                # Creation of the mask
-                output_mask = np.zeros(annot_vol_dim, dtype="float32")
-                output_mask[np.isin(annot_vol, id_list[i])] = 1
-
-                # Masking the anatomical data
-                masked_anat = annat_vol * output_mask
-
-                # Writing of the output images
-                output_mask_path = os.path.join(output_mask_folder, str(id_list[i]) + "_mask.nrrd")
-                write_img(output_mask_path, output_mask)
-                output_masked_annat = os.path.join(output_masked_annat_folder, str(id_list[i]) + "_masked.nrrd")
-                write_img(output_masked_annat, masked_anat)
+    # Writing the output result
+    write_img(output_file_path, output_vol)
 
     return
 
@@ -121,20 +102,15 @@ def mask_id_annot(input_annot_file_path = "", input_annatomical_data_path = "", 
 
 # Command to system
 input_annot_file_path = os.path.abspath(sys.argv[1])
-input_annatomical_data_path = os.path.abspath(sys.argv[2])
-input_hier_id_folder = os.path.abspath(sys.argv[3])
-output_mask_folder = os.path.abspath(sys.argv[4])
-output_masked_annat_folder = os.path.abspath(sys.argv[5])
+input_hier_id_file_list = os.path.abspath(sys.argv[2])
+output_file_path = os.path.abspath(sys.argv[3])
 
-
-print("Starting mask_id_annot_whole_vol...")
+print("Starting mask_id_annot...")
 print("input_annot_file_path:", input_annot_file_path)
-print("input_annatomical_data_path:", input_annatomical_data_path)
-print("input_hier_id_folder:", input_hier_id_folder)
-print("output_mask_folder:", output_mask_folder)
-print("output_masked_annat_folder:", output_masked_annat_folder)
+print("input_hier_id_file_list:", input_hier_id_file_list)
+print("output_file_path:", output_file_path)
 
 print("Processing...")
-mask_id_annot(input_annot_file_path, input_annatomical_data_path, input_hier_id_folder, output_mask_folder, output_masked_annat_folder)
+mask_id_annot(input_annot_file_path, input_hier_id_file_list, output_file_path)
 print("Done: annotation file successfully masked with id values")
 # =============================================================================================
